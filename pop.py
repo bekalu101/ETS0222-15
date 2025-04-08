1b35ffbb8be4800f8bb56f20348168870a206d7f
bekaludebebe <bekaludebebe4@gmail.com>
2025-04-08 23:43:59 +0300

    first commit

---
+++ pop.py
@@ -0,0 +1,9 @@
+colors = ["red", "green", "blue"]
+last_color = colors.pop()
+print(last_color)     # Output: blue
+print(colors)         # Output: ['red', 'green']
+
+# Popping from a specific index
+first_color = colors.pop(0)
+print(first_color)    # Output: red
+print(colors)         # Output: ['green']
